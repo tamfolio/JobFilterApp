1e54810be14898999b5f9f538ece93fef75b81e8
Tamilore <tamiloreolaniyi@gmail.com>
2023-09-11 15:00:33 +0100

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <div className='w-[100%]'>
-      <img src="/assets/bg-header-desktop.svg" alt="" className='bg-[#5da5a4] w-[100%]' />
+      <img src="/assets/bg-header-desktop.svg" alt="" className='bg-[#5da5a4] w-[100%] hidden lg:block' />
+      <img src="/assets/bg-header-mobile.svg" alt="" className='bg-[#5da5a4] w-[100%] lg:hidden' />
       <Jobs/>
       </div>
   )
--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -7,17 +7,18 @@ function Jobs() {
       {Placement.map((jobs) => (
         <div
           key={jobs.id}
-          className="w-[80%] h-[150px] bg-[#fff] flex items-center justify-between mb-[20px] px-[20px] py-[10px]"
+          className="w-[80%] h-[280px] md:h-[150px] bg-[#fff] flex flex-col md:flex-row md:items-center md:justify-between mb-[20px] pl-[25px] py-[10px] md:px-[20px] md:py-[10px] rounded-[15px]"
         >
-          <div className="flex gap-[10px] items-center justify-center">
+          <div className="flex md:flex-row flex-col gap-[10px] items-start justify-start md:items-center md:justify-center">
             <img src={jobs.logo} alt="" />
             <div>
                 <div className="flex gap-[10px]">
                 <p className="text-[#619392] font-bold">{jobs.company}</p>
-                {jobs.new && <div>NEW!</div>}
-                {jobs.featured && <div>FEATURED</div>}
+                {jobs.new && <p className="text-[#fff] bg-[#5fa9a8] font-bold rounded-[15px] px-[8px] py-[2px] text-[15px]">NEW!</p>}
+                {jobs.featured && <p className="text-[#fff] bg-[#283734] font-bold rounded-[15px] px-[8px] py-[2px] text-[15px]">FEATURED</p>}
                 </div>
             <p className="text-black font-bold text-[18px]">{jobs.position}</p>
+           
             <ul className="flex gap-[30px]">
                 <li className="text-[#a5abab]">{jobs.postedAt} </li>
                 <li className="list-disc text-[#a5abab]">{jobs.contract}</li>
@@ -26,6 +27,7 @@ function Jobs() {
             </div>
             
           </div>
+          <div className="w-[250px] h-[2px] bg-[#cacaca] md:hidden my-[20px]"></div>
           <div className="flex gap-[10px]">
           {jobs.languages.map((language, index) => (
               <p key={index} className="text-[#619392] bg-[#eef7f6] px-[8px] py-[5px] font-bold">{language}</p>
